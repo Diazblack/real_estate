dbf7bc8a2482e6b0a24cd0a077229d33c6b12fb2
Cesar Jolibois <diazcd85@gmail.com>
2018-08-29 15:01:47 -0600

    4 iteration complete WOOOOOOOOOHHHHHHOOOOOO

--- lib/house.rb
+++ lib/house.rb
@@ -34,4 +34,18 @@ class House
     number = @price[1..large].to_f
     (number / self.area).round(2)
   end
+
+  def rooms_sorted_by_area
+    @rooms.sort_by do |room|
+      room.area
+    end.reverse
+  end
+
+  def rooms_sorted_by_category
+    sorted_by_category ={}
+    @rooms.each do |cate|
+      sorted_by_category[cate.category] = rooms_from_category(cate.category)
+    end
+    sorted_by_category
+  end
 end
